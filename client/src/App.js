dd848e276ae06fae83fe154eab90c5e175a37c87
Muhammad Shahzaib <mohdshahzaib24@gmail.com>
2020-11-28 00:44:24 +0500

    connected to server

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import { ApolloProvider } from '@apollo/client';
+
+import client from './config/gql_server';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ApolloProvider client={client}>
+      <div>
+        <h2>My first Apollo app 🚀</h2>
+      </div>
+    </ApolloProvider>
   );
 }
 
