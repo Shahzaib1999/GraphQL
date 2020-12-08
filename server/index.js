510b057eb53dc06a9bb88c6e3faae3964b2eae61
Muhammad Shahzaib <mohdshahzaib24@gmail.com>
2020-12-09 01:28:47 +0500

    added mutation

--- server/index.js
+++ server/index.js
@@ -1,24 +1,24 @@
 const { ApolloServer, gql } = require('apollo-server');
 
 const studentsData = [
-    {
-        "id": 1,
-        "name": "Ammar",
-        "email": "ammar@gmail.com",
-        "age": 22
-    },
-    {
-        "id": 2,
-        "name": "Ahmad",
-        "email": "ahmad@gmail.com",
-        "age": 25
-    },
-    {
-        "id": 3,
-        "name": "Zain",
-        "email": "zain@gmail.com",
-        "age": 18
-    }
+  {
+    "id": 1,
+    "name": "Ammar",
+    "email": "ammar@gmail.com",
+    "age": 22
+  },
+  {
+    "id": 2,
+    "name": "Ahmad",
+    "email": "ahmad@gmail.com",
+    "age": 25
+  },
+  {
+    "id": 3,
+    "name": "Zain",
+    "email": "zain@gmail.com",
+    "age": 18
+  }
 ]
 
 const typeDefs = gql`
@@ -29,15 +29,43 @@ const typeDefs = gql`
     age: Int
   }
 
+  input StdInput {
+    id: Int
+    name: String
+    email: String
+    age: Int
+  }
+
   type Query {
     students: [Student]
+  }
+
+  type Mutation {
+    addStudent(input: StdInput): Student
   }
 `;
 
 const resolvers = {
-    Query: {
-        students: () => studentsData
-    },
+  Query: {
+    students: () => studentsData
+  },
+
+  Mutation: {
+    addStudent: (e, { input }) => {
+      studentsData.push({
+        id: input.id,
+        name: input.name,
+        email: input.email,
+        age: input.age
+      })
+      return {
+        id: input.id,
+        name: input.name,
+        email: input.email,
+        age: input.age
+      }
+    }
+  }
 };
 
 const server = new ApolloServer({ typeDefs, resolvers });
